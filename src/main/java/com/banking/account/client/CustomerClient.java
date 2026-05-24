f426106734bab1b0a765264869603711d2de89cc
Srinjoy Das <112161371+joy101020@users.noreply.github.com>
2026-05-24 19:15:55 +0530

    feature-account-minor-changes (#1)
    pomxml
    open feign changes

--- src/main/java/com/banking/account/client/CustomerClient.java
+++ src/main/java/com/banking/account/client/CustomerClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "customer", url = "http://localhost:8081")
+@FeignClient(name = "CUSTOMER-SERVICE")
 public interface CustomerClient {
 
     @GetMapping("/customer/{customerId}")
